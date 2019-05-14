71eaeb403dbeb0bf3a2696a1467e920bba9e0974
sambapython@gmail.com <sambapython@gmail.com>
2019-05-14 09:25:10 +0530

    2019-05-14 python features, anaconda installtion, variables expressions and statements.

---
+++ a.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt
+x=[1,2,3,4,5]
+y=[23,45,67,4,6]
+plt.plot(x,y)
+plt.show()
---
+++ hello.py
@@ -0,0 +1,7 @@
+a=1000
+b=2000
+c=a+b
+print("res=",c)
+print(a,id(a),type(a))
+print(b, id(b),type(b))
+print(c, id(c),type(c))
